e01dc28eed22b99eaa6b071cca7b1c1fa149c635
Tamilmozhi Gunasekar <tamilmozhi.gunasekar@gmail.com>
2020-07-12 23:23:58 +0530

    Add cache interceptor to examples

---
+++ examples/services/http-cache.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpCacheService {
+
+  private requests: any = {};
+
+  constructor() { }
+
+  put(url: string, response: HttpResponse<any>): void {
+    this.requests[url] = response;
+  }
+
+  get(url: string): HttpResponse<any> | undefined {
+    return this.requests[url];
+  }
+
+  invalidateUrl(url: string): void {
+    if (this.requests[url]) {
+      this.requests[url] = undefined;
+    }
+  }
+
+  invalidateCache(): void {
+    this.requests = {};
+  }
+}
