c91e5178847b149274bdd5c13a8ae3f5db16c894
Tamilmozhi Gunasekar <tamilmozhi.gunasekar@gmail.com>
2020-07-31 22:49:10 +0530

    Add login component

--- practice/Angular-GettingStarted/src/app/user/user.module.ts
+++ practice/Angular-GettingStarted/src/app/user/user.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent }
+    ])
   ]
 })
 export class UserModule { }
